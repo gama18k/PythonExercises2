ec9c300b544545f804d01be53de023dc3e617be7
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-10 15:08:03 -0300

    Create 3FemininoMasculino.py

---
+++ 3FemininoMasculino.py
@@ -0,0 +1,10 @@
+# 3. Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+print("Solução do exercício 3:")
+letra = str(input("digite F ou M:")).upper()
+if letra == "F":
+     print("F - Feminino")
+ elif letra == "M":
+         print("M - Masculino")
+ else:
+         print("Sexo Inválido")
