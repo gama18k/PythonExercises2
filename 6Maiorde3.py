94c4906b0a70254e618af0426774aaaf1810a297
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-10 15:11:27 -0300

    Create 6Maiorde3.py

---
+++ 6Maiorde3.py
@@ -0,0 +1,8 @@
+# 6. Faça um Programa que leia três números e mostre o maior deles.
+print("Solução do exercício 6:")
+num1 = int(input("digite o primeiro número: "))
+num2 = int(input("digite o segundo número: "))
+num3 = int(input("digite o terceiro número: "))
+
+maiorNum = max(num1, num2, num3)
+print(f"O maior número é: ", maiorNum)
