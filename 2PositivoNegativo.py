0684e73f94af2861ac0db0f05ff4a2d24f4bc078
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-10 15:07:18 -0300

    Create 2PositivoNegativo.py

---
+++ 2PositivoNegativo.py
@@ -0,0 +1,8 @@
+# 2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+print("Solução do exercício 2:")
+ num = int(input("digite um número:"))
+ if (num>0):
+     print("valor positivo")
+ else:
+     print("valor negativo")
