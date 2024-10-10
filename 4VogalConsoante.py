b093c99d2e9f520fcd4c553f7b524502abac8b7a
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-10 15:09:37 -0300

    Create 4VogalConsoante.py

---
+++ 4VogalConsoante.py
@@ -0,0 +1,8 @@
+# 4. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+print("Solução do exercício 4:")
+vogal = ["a", "e", "i", "o", "u"]
+letra = input(str("digite uma letra: "))
+if letra in vogal:
+     print("letra é vogal")
+else:
+     print("a letra é consoante")
